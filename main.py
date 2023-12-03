1723aaee0539780478e32795960c06933b9d75dc
MarioStack <javol33@gmail.com>
2023-12-03 10:28:44 +0200

    day3

--- main.py
+++ main.py
@@ -6,13 +6,15 @@ ABSOLUTE_PATH = os.path.dirname(__file__)
 
 FILE_DIC = {
     1: f"{ABSOLUTE_PATH}{SEP}files{SEP}input_day1.txt",
-    2: f"{ABSOLUTE_PATH}{SEP}files{SEP}input_day2.txt"
+    2: f"{ABSOLUTE_PATH}{SEP}files{SEP}input_day2.txt",
+    3: f"{ABSOLUTE_PATH}{SEP}files{SEP}input_day3.txt"
 }
 
 
 def main():
-    print(code.day_one(FILE_DIC[1]))
-    print(code.day_two(FILE_DIC[2]))
+    #print(code.day_one(FILE_DIC[1]))
+    #print(code.day_two(FILE_DIC[2]))
+    print(code.day_three(FILE_DIC[3]))
 
 
 if __name__ == '__main__':
